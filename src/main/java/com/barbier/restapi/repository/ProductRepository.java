861f44a88ffad80d1fa7190854261277d3de1187
carlos barbier <carlos.barbier@outlook.com>
2020-01-22 22:21:53 +0000

    Product repository

---
+++ src/main/java/com/barbier/restapi/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.barbier.restapi.repository;
+
+import com.barbier.restapi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
